3cdb67173e95fd46398a791cd3a73da38b7a952c
Felix Droop <felix.droop@fu-berlin.de>
2022-12-19 13:57:33 +0100

    improve script for bin file

--- MG-HIBF/create_simple_bin_file.py
+++ MG-HIBF/create_simple_bin_file.py
@@ -1,14 +1,15 @@
 import os
 import sys
 import csv
+from pathlib import Path
 
-folder_name = sys.argv[1]
+folder_name = Path(sys.argv[1])
 fasta_file_endings = [".fa", ".fna", ".fasta"]
 
 is_fasta_file = lambda f: any(f.endswith(end) for end in fasta_file_endings)
 fasta_filenames = filter(lambda f: is_fasta_file(f), os.listdir(folder_name))
 
-to_path = lambda filename: folder_name + filename
+to_path = lambda filename: folder_name / filename
 fasta_paths = map(to_path, fasta_filenames)
 
 bin_size = int(sys.argv[2]) if len(sys.argv) > 2 else 1
