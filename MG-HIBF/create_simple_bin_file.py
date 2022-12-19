f94787b1ce2df40e17521030ffa85713ca27d14c
Felix Droop <felix.droop@fu-berlin.de>
2022-12-19 13:04:25 +0100

    Add litttle script to create a simple bin file

---
+++ MG-HIBF/create_simple_bin_file.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import csv
+
+folder_name = sys.argv[1]
+fasta_file_endings = [".fa", ".fna", ".fasta"]
+
+is_fasta_file = lambda f: any(f.endswith(end) for end in fasta_file_endings)
+fasta_filenames = filter(lambda f: is_fasta_file(f), os.listdir(folder_name))
+
+to_path = lambda filename: folder_name + filename
+fasta_paths = map(to_path, fasta_filenames)
+
+bin_size = int(sys.argv[2]) if len(sys.argv) > 2 else 1
+with open("bins.tsv", "w+") as f:
+    writer = csv.writer(f, delimiter='\t')
+    for i, fasta_path in enumerate(fasta_paths):
+        writer.writerow([fasta_path, int(i / bin_size)])
