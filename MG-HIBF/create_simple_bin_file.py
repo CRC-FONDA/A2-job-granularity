3d4ba7717ae8c4b8119cf371990df4a07ea22360
Manuel Zschaebitz <manuez42@zedat.fu-berlin.de>
2023-03-24 10:36:43 +0100

    bug fix

--- MG-HIBF/create_simple_bin_file.py
+++ MG-HIBF/create_simple_bin_file.py
@@ -16,8 +16,11 @@ fasta_paths = map(to_path, fasta_filenames)
 
 nodelist = ["cmp247","cmp248","cmp201","cmp202","cmp203","cmp204","cmp205","cmp213","cmp214","cmp215","cmp216","cmp217","cmp218","cmp219","cmp220","cmp230"]
 bin_size = int(sys.argv[2]) if len(sys.argv) > 2 else 1
+nodedict = {}
+for i in range(bin_size):
+    nodedict[i] = random.choice(nodelist)
+
 with open("bins.tsv", "w+") as f:
     writer = csv.writer(f, delimiter='\t')
     for i, fasta_path in enumerate(fasta_paths):
-        node = find_node(bin_size)
-        writer.writerow([fasta_path, int(i / bin_size), random.choice(nodelist)])
+        writer.writerow( [fasta_path, int(i/bin_size),  nodedict[int(i/bin_size)]] )
