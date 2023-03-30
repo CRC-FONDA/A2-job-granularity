a25993d10bb209ae46f3830162a56892c45fbc43
Manuel Zschaebitz <manuez42@zedat.fu-berlin.de>
2023-03-30 16:19:18 +0200

    test test test

--- MG-HIBF/Scripts/tests.py
+++ MG-HIBF/Scripts/tests.py
@@ -1,16 +1,22 @@
 #Testfile for changing Parameters in the Workflow
 
-#import subprocess
-#import pandas as pd
-#import numpy as np 
+import subprocess
+import pandas as pd
+import numpy as np
+import random as r
 
-#def NodeList_offline():
-#    return pd.read_csv("../../NodeList.csv", header=0, index_col=False)
+#Loading available Nodes
+# def NodeList():
+#     cmd = ["bash", "-c", "sinfo --long --Node"]
+#     result = subprocess.run(cmd, stdout=subprocess.PIPE)
+#     return pd.read(result.decode('utf-8'))
 
+# def NodeList_offline():
+#     return pd.read_csv("../../NodeList.csv", header=0, index_col=False)
 
-#def function (bin_nr):
-#    list_of_Nodes = NodeList_offline()
-#    return list_of_Nodes(NodeName[bin_nr])
+# def match_node (bin_nr):
+#     list_of_Nodes = NodeList_offline()
+#     return list_of_Nodes(NodeName[bin_nr])
 
 
 # def NodeList():
