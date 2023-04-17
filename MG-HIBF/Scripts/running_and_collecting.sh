df3b028a4d2392c11d31e07707f6d132813ecaa8
Manuel Zschaebitz <manuez42@zedat.fu-berlin.de>
2023-04-17 08:28:20 +0200

    wip

--- MG-HIBF/Scripts/running_and_collecting.sh
+++ MG-HIBF/Scripts/running_and_collecting.sh
@@ -9,7 +9,7 @@ files5='/buffer/ag_abi/manuel/fonda/genomes/bacteria_88G/files/'
 files6='/buffer/ag_abi/manuel/fonda/genomes/human_9G/files/'
 files7='/buffer/ag_abi/manuel/fonda/genomes/viral_500M/files/'
 
-path_to_collect=~/fonda/A2-job-granularity/MG-HIBF/collect/
+path_to_collect='~/fonda/A2-job-granularity/MG-HIBF/collect/'
 
 ### running
 function run(){
@@ -42,8 +42,6 @@ mv bwa-mem2-index* collect/
 mv data/bins.tsv collect/
 mv nodes.csv collect/
 python Scripts/building_result.py $path_to_collect 500
-
-
 
 #run $files7 20
 #collecting '9G'
