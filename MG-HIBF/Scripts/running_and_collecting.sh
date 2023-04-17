1a2f86acfb5b7f296f71446ce3a08bd985fee663
Manuel Zschaebitz <manuez42@zedat.fu-berlin.de>
2023-04-17 08:16:47 +0200

    wip

--- MG-HIBF/Scripts/running_and_collecting.sh
+++ MG-HIBF/Scripts/running_and_collecting.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
 ### Path to files ###
-files1='/buffer/ag_abi/manuel/fonda/genomes/archaea_1,4G/'
-files2='/buffer/ag_abi/manuel/fonda/genomes/bacteria_125G/'
-files3='/buffer/ag_abi/manuel/fonda/genomes/bacteria_30G/'
-files4='/buffer/ag_abi/manuel/fonda/genomes/bacteria_58G/'
-files5='/buffer/ag_abi/manuel/fonda/genomes/bacteria_88G/'
-files6='/buffer/ag_abi/manuel/fonda/genomes/human_9G/'
-files7='/buffer/ag_abi/manuel/fonda/genomes/viral_500M/'
+files1='/buffer/ag_abi/manuel/fonda/genomes/archaea_1,4G/files/'
+files2='/buffer/ag_abi/manuel/fonda/genomes/bacteria_125G/files/'
+files3='/buffer/ag_abi/manuel/fonda/genomes/bacteria_30G/files/'
+files4='/buffer/ag_abi/manuel/fonda/genomes/bacteria_58G/files/'
+files5='/buffer/ag_abi/manuel/fonda/genomes/bacteria_88G/files/'
+files6='/buffer/ag_abi/manuel/fonda/genomes/human_9G/files/'
+files7='/buffer/ag_abi/manuel/fonda/genomes/viral_500M/files/'
 
 path_to_collect=~/fonda/A2-job-granularity/MG-HIBF/collect/
 
