04c122b2b5a2bf768719f7032860296907edfcd5
Manuel Zschaebitz <manuez42@zedat.fu-berlin.de>
2023-04-18 07:52:31 +0200

    wip

--- MG-HIBF/create_simple_bin_file.py
+++ MG-HIBF/create_simple_bin_file.py
@@ -13,7 +13,7 @@ is_fasta_file = lambda f: any(f.endswith(end) for end in fasta_file_endings)
 fasta_filenames = filter(lambda f: is_fasta_file(f), os.listdir(folder_name))
 
 to_path = lambda filename: folder_name / filename
-fasta_paths = map(to_path, fasta_filenames)
+fasta_paths = list(map(to_path, fasta_filenames))
 
 bin_size = files_per_bin
 ### check bin_size for viability (bin too big? too small? number of files per bin?)
