76940898b3cfd78ef3df2907152d1bba791651f5
manuez42 <manuez42@alnus.imp.fu-berlin.de>
2023-05-30 13:22:53 +0200

    fixup

--- MG-HIBF/Scripts/running_and_collecting.sh
+++ MG-HIBF/Scripts/running_and_collecting.sh
@@ -13,25 +13,25 @@ export PATH="~/miniconda3/envs/snakemake/lib/python3.11/isite-packages/pandas:$P
 
 
 ### Path to files ###
-name1='archaea_1,4G'
+name1='archaea'
 files1='/data/scratch/manuez42/genomes/archaea_1,4G/files/'
 
-name2='bacteria_125G'
+name2='bacteria'
 files2='/data/scratch/manuez42/genomes/bacteria_125G/files/'
 
-name3='bacteria_30G'
+name3='bacteria'
 files3='/data/scratch/manuez42/genomes/bacteria_30G/files/'
 
-name4='bacteria_58G'
+name4='bacteria'
 files4='/data/scratch/manuez42/genomes/bacteria_58G/files/'
 
-name5='bacteria_88G'
+name5='bacteria'
 files5='/data/scratch/manuez42/genomes/bacteria_88G/files/'
 
-name6='human_9G'
+name6='human'
 files6='/data/scratch/manuez42/genomes/human_9G/files/'
 
-name7='viral_500M'
+name7='viral'
 files7='/data/scratch/manuez42/genomes/viral_500M/files/'
 
 path_to_collect='collect/'
@@ -43,15 +43,16 @@ run_and_collect(){
     mv bins.tsv data/
     snakemake --use-conda -s Snakefile --cluster 'sbatch -t 120 --nodelist={resources.nodelist} --partition="big"' -j 100 --latency-wait 600
     
-    ## $3 path to collect
-    ## $4 DataSize in GB 
-    ## $5 is Name
+    
     mkdir -p collect
 
     mv slurm* collect/
     mv bwa-mem2-index* collect/
     mv node*.csv collect/
 
+    # $3 path to collect
+    ## $4 DataSize in GB 
+    ## $5 is Name
     python Scripts/building_result.py $3 $4 $5
     mv result*.csv final/
     
