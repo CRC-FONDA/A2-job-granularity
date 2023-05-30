09bc9064b7c53d3237e754cbb098f5f9cf9efe27
manuez42 <manuez42@alnus.imp.fu-berlin.de>
2023-05-30 13:36:24 +0200

    fixup

--- MG-HIBF/Scripts/setup_tools.sh
+++ MG-HIBF/Scripts/setup_tools.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/bash
 
-cd ..
-mkdir tools
-cd tools
+cd ../../tools
 
 #install raptor
 git clone --recurse-submodules https://github.com/seqan/raptor
@@ -10,7 +8,7 @@ cd raptor
 mkdir build
 cd build
 cmake .. -DCMAKE_CXX_COMPILER=/home/felid99/gcc/gcc-11.3.0/bin/g++-11.3.0 -DCMAKE_C_COMPILER=/home/felid99/gcc/gcc-11.3.0/bin/gcc-11.3.0
-make -j10 
+make -j30
 #change j10 to the number of cores you want to use 10 -> 10 cores
 #(it's polite to take max half the number of available cores)
 
